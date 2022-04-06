00fcd8df0ace69b4411a35fa266de1eaa65a705e
Mya Rios <myarios23@gmail.com>
2022-04-05 23:31:34 -0400

    adding api to check if user entered word is valid

--- js/index.js
+++ js/index.js
@@ -16,6 +16,8 @@ class Letter{
 
 class Word{
     letters = [];
+    isValid = false;
+
     constructor(letters){
         this.letters = letters;
     }
@@ -27,6 +29,43 @@ class Word{
             string += this.letters[i].display();
         }
         return string;
+    }
+
+    toString(){
+        let string = "";
+        this.letters.forEach(letter => string+= letter.value);
+
+        return string;
+    }
+
+    /*Check if a word is a real dictionary word 
+    (Takes a query parameter called word which denotes the word to check) */    
+    async checkIsValid(attempts){
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Host': 'wordle-creator-tools.p.rapidapi.com',
+                'X-RapidAPI-Key': 'c5c5487ec6mshfa9eea1774ea84fp1b6491jsn97d7ea39604c'
+            }
+        };
+        
+        await fetch(`https://wordle-creator-tools.p.rapidapi.com/check-word?word=${this.toString()}`, options)
+            .then(response => response.json())
+            .then(response => {
+                if(response.result === true){
+                    console.log("The word entered was valid");
+                    this.isValid = true;
+
+                    let currAttemptRow = document.getElementById("attempt_" + attempts);
+                    currAttemptRow.innerHTML = this.display();
+
+                }else{
+                    // inform user that entry is not valid word
+                    console.log("The word entered was not valid");
+                }
+            })
+            .catch(err => console.error(err));        
     }
 }
 
@@ -70,39 +109,37 @@ class Game{
         gameBoard.appendChild(submit);
 
         submit.addEventListener("click", () => {
-            if(submitGuess(input, this.attempts)){
-                this.attempts++;
-            };
+            this.submitGuess(input);
+            input.value = "";
         }); 
+    }
+
+    submitGuess(input){
+        if(input.value.length < 5){
+            // inform user that they need to enter text with more than 5 characters
+            console.log("Need at least 5 letters for each attempt");
+            return false;
+    
+        }else{
+            console.log("You inputted: " + input.value);    // DEBUG
+    
+            let guess = convertStringToLetterArray(input.value.split(''));
+            let guessWordObj = new Word(guess);
+    
+            guessWordObj.checkIsValid(this.attempts)
+            .then(()=>{
+                    if(guessWordObj.isValid == true){
+                        this.attempts++;
+                    }
+                }
+            )
+            .catch(err => console.log(err));
+        }
     }
 }
 
 function  convertStringToLetterArray(stringArray){
     return stringArray.map((item)=> new Letter(item));
-}
-
-function submitGuess(input, attempts){
-
-    if(input.value.length < 5){
-        // inform user that they need to enter text with more than 5 characters
-        console.log("Need at least 5 letters for each attempt");
-        return false;
-
-    }else{
-        console.log("There was text in the input box");
-
-        let guess = convertStringToLetterArray(input.value.split(''));  // may want to handle elsewhere
-        console.log(guess);     // DEBUG
-
-        let guessWordObj = new Word(guess);
-        console.log(guessWordObj);  // DEBUG
-
-        let currAttemptRow = document.getElementById("attempt_" + attempts);
-        currAttemptRow.innerHTML = guessWordObj.display();
-        input.value = "";
-
-        return true;
-    }
 }
 
 function createGameBoard(){
